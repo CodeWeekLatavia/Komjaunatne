93c60b05d7330002f44207f0323f1e3a566343ec
Krišjānis Petručeņa <krisjanispetrucena@gmail.com>
2021-05-29 22:17:25 +0300

    Update landing page

--- wexp/pages/_app.tsx
+++ wexp/pages/_app.tsx
@@ -4,15 +4,26 @@ import "bootstrap/dist/css/bootstrap.css";
 
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+
+import Navigation from "../components/Navigation";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+
       <Head>
+        <title>WExp</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="keywords" content="Next.js, Bootstrap 5.0, Typescript" />
-        <meta name="author" content="Krišjānis Petručeņa, Anete Gūtmane, Ārtūrs Kļaviņš, Rem Vasilenko, Eduards Žeiris, Raivo Logins" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="author"
+          content="Krišjānis Petručeņa, Anete Gūtmane, Ārtūrs Kļaviņš, Rem Vasilenko, Eduards Žeiris, Raivo Logins"
+        />
       </Head>
+      <Navigation/>
       <Component {...pageProps} />
     </>
   );
