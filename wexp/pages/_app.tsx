892d2deba1f5781b63fc757b0bf7cbfea0f082ea
Krišjānis Petručeņa <krisjanispetrucena@gmail.com>
2021-05-29 13:14:39 +0300

    Add more meta information

--- wexp/pages/_app.tsx
+++ wexp/pages/_app.tsx
@@ -3,13 +3,15 @@ import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.css";
 
 import type { AppProps } from "next/app";
-import Head from 'next/head'
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="keywords" content="Next.js, Bootstrap 5.0, Typescript" />
+        <meta name="author" content="Krišjānis Petručeņa, Anete Gūtmane, Ārtūrs Kļaviņš, Rem Vasilenko, Eduards Žeiris, Raivo Logins" />
       </Head>
       <Component {...pageProps} />
     </>
