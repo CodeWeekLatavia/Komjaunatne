ba1136d6a64af3f2cd8e7a4907c7d1adbbc78507
KrisjanisP <krisjanispetrucena@gmail.com>
2021-09-30 11:41:54 +0300

    Mobile Menu

--- wexp/components/Navigation.tsx
+++ wexp/components/Navigation.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Link from "next/link";
 import { withRouter, NextRouter } from "next/router";
 
@@ -14,43 +15,89 @@ const companySectionPath = '/company';
 const loginSectionPath = '/login';
 const dashboardSectionPath = '/dashboard'
 
+const navMenuId = ''
+
+let isYouthSection = (props: { router: NextRouter }) => props.router.pathname.includes(youthSectionPath);
+let isCompanySection = (props: { router: NextRouter }) => props.router.pathname.includes(companySectionPath);
+let isLoginSection = (props: { router: NextRouter }) => props.router.pathname.includes(loginSectionPath);
+let isDashboardSection = (props: { router: NextRouter }) => props.router.pathname.includes(dashboardSectionPath);
+
+
 function Navigation(props: { router: NextRouter }) {
-
-  let isYouthSection = (props: { router: NextRouter }) => props.router.pathname.includes(youthSectionPath);
-  let isCompanySection = (props: { router: NextRouter }) => props.router.pathname.includes(companySectionPath);
-  let isLoginSection = (props: { router: NextRouter }) => props.router.pathname.includes(loginSectionPath);
-  let isDashboardSection = (props: { router: NextRouter }) => props.router.pathname.includes(dashboardSectionPath);
-
-  let AuthUser = useAuthUser();
-  let auth = AuthUser.id !== null;
-
+  let [mobileMenuState, setMobileMenuState] = useState(false);
+  let authUser = useAuthUser();
+  console.log(mobileMenuState);
   return (
-    <NavContainer>
-      <NavLogo />
-      <div className="d-none d-md-block">
-        <div className="d-flex flex-wrap justify-content-center py-2 py-md-2 align-items-center">
-          {!auth && !isLoginSection(props) ? <LoginBtn /> : !auth ? <HomeBtn /> : null}
-          {!auth && !isYouthSection(props) ? <YouthBtn /> : !auth ? <HomeBtn /> : null}
-          {!auth && !isCompanySection(props) ? <CompanyBtn /> : !auth ? <HomeBtn /> : null}
-          {auth && !isDashboardSection(props) ? <DashboardBtn /> : null}
-          {AuthUser.email ? <><DebugInfo text={AuthUser.email} /> <LogoutBtn /></> : null}
+    <>
+      <NavContainer>
+        <NavLogo />
+        <div className="d-none d-md-block">
+          <NavMenuList authUser={authUser} router={props.router} />
+        </div>
+        <div className="d-block d-md-none p-2" onClick={() => setMobileMenuState(!mobileMenuState)}>
+          <NavMenuIcon />
+        </div>
+      </NavContainer>
+      {mobileMenuState ?
+        <NavMenuPopup authUser={authUser} router={props.router} setMobileMenuState={setMobileMenuState}/> : null}
+    </>
+  );
+}
+
+const NavMenuList = (props: { authUser, router: NextRouter }) => {
+  let authUser = props.authUser;
+  let auth = authUser.id !== null;
+  return (
+    <div className="d-flex flex-wrap justify-content-center py-2 py-md-2 align-items-center">
+      {!auth && !isLoginSection(props) ? <LoginBtn /> : !auth ? <HomeBtn /> : null}
+      {!auth && !isYouthSection(props) ? <YouthBtn /> : !auth ? <HomeBtn /> : null}
+      {!auth && !isCompanySection(props) ? <CompanyBtn /> : !auth ? <HomeBtn /> : null}
+      {auth && !isDashboardSection(props) ? <DashboardBtn /> : null}
+      {authUser.email ? <><DebugInfo text={authUser.email} /> <LogoutBtn /></> : null}
+    </div>
+  );
+}
+
+const NavMenuPopup = (props: { authUser, router: NextRouter, setMobileMenuState }) => {
+  let authUser = props.authUser;
+  let auth = authUser.id !== null;
+  let btnAdditionalClasses = "w-100 text-center"
+  let closeMenu = function (e) {
+    console.log(e);
+    if (e.target !== e.currentTarget)
+    return;
+    props.setMobileMenuState(false);
+  }
+  return (
+    <div className={"position-fixed vh-100 vw-100 d-md-none"} onClick={closeMenu} style={{ zIndex: 4, backgroundColor: "rgba(0,0,0,0.5)" }}>
+      <div className="container d-flex h-75 my-5">
+        <div className="card w-100">
+          <div className="card-body">
+          <div className="card-title">
+            <h1>Navigation Menu</h1>
+          </div>
+            <div className="d-flex flex-column justify-content-center py-2 py-md-2 align-items-center
+                            justify-content-evenly h-100">
+              {!auth && !isLoginSection(props) ? <LoginBtn addClassName={btnAdditionalClasses} callback={()=>props.setMobileMenuState(false)} /> : !auth ? <HomeBtn addClassName={btnAdditionalClasses} callback={()=>props.setMobileMenuState(false)} /> : null}
+              {!auth && !isYouthSection(props) ? <YouthBtn addClassName={btnAdditionalClasses} callback={()=>props.setMobileMenuState(false)} /> : !auth ? <HomeBtn addClassName={btnAdditionalClasses} callback={()=>props.setMobileMenuState(false)} /> : null}
+              {!auth && !isCompanySection(props) ? <CompanyBtn addClassName={btnAdditionalClasses} callback={()=>props.setMobileMenuState(false)} /> : !auth ? <HomeBtn addClassName={btnAdditionalClasses} callback={()=>props.setMobileMenuState(false)} /> : null}
+              {auth && !isDashboardSection(props) ? <DashboardBtn addClassName={btnAdditionalClasses} callback={()=>props.setMobileMenuState(false)} /> : null}
+              {authUser.email ? <><DebugInfo text={authUser.email} /> <LogoutBtn /></> : null}
+            </div>
+          </div>
         </div>
       </div>
-      <div className="d-block d-md-none">
-        <NavMenuIcon />
-      </div>
-    </NavContainer>
+    </div>
   );
 }
 
 const navButtonClasses = "rounded p-1 px-3 border m-1 fw-normal h5 mx-2 text-decoration-none";
-
-let YouthBtn = () => (<Link href={youthSectionPath}><a className={`${navButtonClasses} light-button`}>For Youth</a></Link>);
-let CompanyBtn = () => (<Link href={companySectionPath}><a className={`${navButtonClasses} dark-button`}>For Companies</a></Link>);
-let HomeBtn = () => (<Link href="/"><a className={`${navButtonClasses} purple-button`}>Home</a></Link>);
-let LoginBtn = () => (<Link href={loginSectionPath}><a className={`${navButtonClasses} white-button`}>Log In</a></Link>);
-let LogoutBtn = () => (<a className={`${navButtonClasses} red-button`} onClick={logOut}>Log Out</a>);
-let DashboardBtn = () => (<Link href={dashboardSectionPath}><a className={`${navButtonClasses} green-button`}>Open Dashboard</a></Link>);
+let YouthBtn = (props) => (<Link href={youthSectionPath}><a className={`${navButtonClasses} light-button ${props.addClassName}`} onClick={props.callback}>For Youth</a></Link>);
+let CompanyBtn = (props) => (<Link href={companySectionPath}><a className={`${navButtonClasses} dark-button ${props.addClassName}`} onClick={props.callback}>For Companies</a></Link>);
+let HomeBtn = (props) => (<Link href="/"><a className={`${navButtonClasses} purple-button ${props.addClassName}`} onClick={props.callback}>Home</a></Link>);
+let LoginBtn = (props) => (<Link href={loginSectionPath}><a className={`${navButtonClasses} white-button ${props.addClassName}`} onClick={props.callback}>Log In</a></Link>);
+let LogoutBtn = (props) => (<a className={`${navButtonClasses} red-button  ${props.addClassName}`} onClick={()=>{props.callback();logOut()}}>Log Out</a>);
+let DashboardBtn = (props) => (<Link href={dashboardSectionPath}><a className={`${navButtonClasses} green-button ${props.addClassName}`} onClick={props.callback}>Open Dashboard</a></Link>);
 let DebugInfo = (props: { text: string }) => (<div className={`${navButtonClasses}`}>{props.text}</div>);
 
 function logOut() {
